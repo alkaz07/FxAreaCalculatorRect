5444d2bd4538921ec3a31401787c9ddd73c7455b
StudentGeyvandov <9327337@mail.ru>
2023-08-15 15:14:08 +0300

    123

--- src/main/java/com/example/fxareacalculator/HelloController.java
+++ src/main/java/com/example/fxareacalculator/HelloController.java
@@ -1,14 +1,42 @@
 package com.example.fxareacalculator;
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.text.DecimalFormat;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    TextField heightTextField;
+
+    @FXML
+    TextField widthTextField;
+
+    @FXML
+    Label sumLabel;
+
+    SimpleDoubleProperty width = new SimpleDoubleProperty();
+    SimpleDoubleProperty height = new SimpleDoubleProperty();
+    SimpleDoubleProperty sum = new SimpleDoubleProperty();
+
+    public void initialize(){
+//        sum.set(width.get()*height.get());
+
+
+        sumLabel.textProperty().bindBidirectional(sum, new DecimalFormat());
+        heightTextField.textProperty().bindBidirectional(height, new DecimalFormat());
+        widthTextField.textProperty().bindBidirectional(width, new DecimalFormat());
+
+        heightTextField.setOnInputMethodTextChanged(actionEvent -> sum.set((width.get()*height.get())));
+        widthTextField.setOnAction(actionEvent -> sum.set((width.get()*height.get())));
+
     }
+
+
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.fxareacalculator {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.fxareacalculator to javafx.fxml;
     exports com.example.fxareacalculator;
 }
